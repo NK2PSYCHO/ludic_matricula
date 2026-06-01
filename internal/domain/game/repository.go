bbb5984a8c4a10292850e73aa376f1f5de587aec
NK2PSCYHO <knightmaresafai@gmail.com>
2026-06-01 09:55:59 +0000

    define game repository struct

--- internal/domain/game/repository.go
+++ internal/domain/game/repository.go
@@ -1,1 +1,11 @@
 package game
+
+import "github.com/google/uuid"
+
+type GameRepository interface {
+	AddGame(game Game) error
+	UpdateGame(id uuid.UUID, game Game) error
+	DeleteGame(id uuid.UUID) error
+	GetGame(id uuid.UUID) (Game, error)
+	GetGames(id uuid.UUID) ([]Game, error)
+}
