5cc17da415abac7735c9a433a309450c05fe431f
NK2PSCYHO <knightmaresafai@gmail.com>
2026-06-01 10:34:57 +0000

    define and implement using placeholders service interfaces for user and game

--- internal/domain/game/service.go
+++ internal/domain/game/service.go
@@ -1,1 +1,45 @@
 package game
+
+import (
+	"github.com/google/uuid"
+)
+
+var _ GameService = (*gameService)(nil)
+
+type GameService interface {
+	AddGame(user Game) error
+	UpdateGame(id uuid.UUID, Game Game) error
+	DeleteGame(id uuid.UUID) error
+	GetGame(id uuid.UUID) (Game, error)
+	GetGames(id uuid.UUID) ([]Game, error)
+}
+
+type gameService struct {
+	repo GameRepository
+}
+
+func NewUserService(repo GameRepository) *gameService {
+	return &gameService{
+		repo: repo,
+	}
+}
+
+func (g *gameService) AddGame(user Game) error {
+	panic("unimplemented")
+}
+
+func (g *gameService) UpdateGame(id uuid.UUID, Game Game) error {
+	panic("unimplemented")
+}
+
+func (g *gameService) DeleteGame(id uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (g *gameService) GetGame(id uuid.UUID) (Game, error) {
+	panic("unimplemented")
+}
+
+func (g *gameService) GetGames(id uuid.UUID) ([]Game, error) {
+	panic("unimplemented")
+}
--- internal/domain/user/service.go
+++ internal/domain/user/service.go
@@ -1,1 +1,45 @@
 package user
+
+import (
+	"github.com/google/uuid"
+)
+
+var _ UserService = (*userService)(nil)
+
+type UserService interface {
+	AddUser(user User) error
+	UpdateUser(id uuid.UUID, user User) error
+	DeleteUser(id uuid.UUID) error
+	GetUser(id uuid.UUID) (User, error)
+	GetUsers(id uuid.UUID) ([]User, error)
+}
+
+type userService struct {
+	repo UserRepository
+}
+
+func NewUserService(repo UserRepository) *userService {
+	return &userService{
+		repo: repo,
+	}
+}
+
+func (u *userService) AddUser(user User) error {
+	panic("unimplemented")
+}
+
+func (u *userService) UpdateUser(id uuid.UUID, user User) error {
+	panic("unimplemented")
+}
+
+func (u *userService) DeleteUser(id uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (u *userService) GetUser(id uuid.UUID) (User, error) {
+	panic("unimplemented")
+}
+
+func (u *userService) GetUsers(id uuid.UUID) ([]User, error) {
+	panic("unimplemented")
+}
