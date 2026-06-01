ca5eda40687e2b1782526810303eed79848352b6
NK2PSCYHO <knightmaresafai@gmail.com>
2026-06-01 09:16:25 +0000

    define user repository

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -1,1 +1,11 @@
 package user
+
+import "github.com/google/uuid"
+
+type UserRepository interface {
+	AddUser(user User) error
+	UpdateUser(id uuid.UUID, user User) error
+	DeleteUser(id uuid.UUID) error
+	GetUser(id uuid.UUID) (User, error)
+	GetUsers(id uuid.UUID) ([]User, error)
+}
