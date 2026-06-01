08a603bad46192902eecee4187df1837dcb9e2e0
NK2PSCYHO <knightmaresafai@gmail.com>
2026-06-01 08:59:21 +0000

    define game struct

--- internal/domain/game/entity.go
+++ internal/domain/game/entity.go
@@ -1,1 +1,17 @@
 package game
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Game struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Publisher string    `json:"publisher"`
+	Year      string    `json:"year"`
+	Platform  string    `json:"platform"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
