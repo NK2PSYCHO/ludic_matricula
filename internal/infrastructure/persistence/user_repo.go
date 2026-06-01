af2358d0f0863de0700840c4d5065503968c3e0c
NK2PSCYHO <knightmaresafai@gmail.com>
2026-06-01 09:55:33 +0000

    define user repo and define placeholder functions to implement user respository

--- internal/infrastructure/persistence/user_repo.go
+++ internal/infrastructure/persistence/user_repo.go
@@ -1,1 +1,40 @@
 package persistence
+
+import (
+	"ludic-matricula/internal/domain/user"
+
+	"github.com/google/uuid"
+	"github.com/uptrace/bun"
+)
+
+var _ user.UserRepository = (*userRepo)(nil)
+
+type userRepo struct {
+	db *bun.DB
+}
+
+func NewUserRepo(db *bun.DB) *userRepo {
+	return &userRepo{
+		db: db,
+	}
+}
+
+func (u *userRepo) AddUser(user user.User) error {
+	panic("unimplemented")
+}
+
+func (u *userRepo) UpdateUser(id uuid.UUID, user user.User) error {
+	panic("unimplemented")
+}
+
+func (u *userRepo) DeleteUser(id uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (u *userRepo) GetUser(id uuid.UUID) (user.User, error) {
+	panic("unimplemented")
+}
+
+func (u *userRepo) GetUsers(id uuid.UUID) ([]user.User, error) {
+	panic("unimplemented")
+}
