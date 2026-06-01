d2687c3da05c1a1c22835bcc95887b53c3299ac9
NK2PSCYHO <knightmaresafai@gmail.com>
2026-06-01 09:58:33 +0000

    define game repo struct and implement game repository using it

--- internal/infrastructure/persistence/game_repo.go
+++ internal/infrastructure/persistence/game_repo.go
@@ -1,1 +1,40 @@
 package persistence
+
+import (
+	"ludic-matricula/internal/domain/game"
+
+	"github.com/google/uuid"
+	"github.com/uptrace/bun"
+)
+
+var _ game.GameRepository = (*gameRepo)(nil)
+
+type gameRepo struct {
+	db *bun.DB
+}
+
+func NewGameRepo(db *bun.DB) *gameRepo {
+	return &gameRepo{
+		db: db,
+	}
+}
+
+func (g *gameRepo) AddGame(game game.Game) error {
+	panic("unimplemented")
+}
+
+func (g *gameRepo) UpdateGame(id uuid.UUID, game game.Game) error {
+	panic("unimplemented")
+}
+
+func (g *gameRepo) DeleteGame(id uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (g *gameRepo) GetGame(id uuid.UUID) (game.Game, error) {
+	panic("unimplemented")
+}
+
+func (g *gameRepo) GetGames(id uuid.UUID) ([]game.Game, error) {
+	panic("unimplemented")
+}
