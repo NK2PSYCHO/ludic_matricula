e8c4d02b4aae2458d323871ebc681e6036c092c0
NK2PSCYHO <knightmaresafai@gmail.com>
2026-06-08 08:35:03 +0000

    define roles for user and add role field to user and userdto struct

--- internal/domain/user/entity.go
+++ internal/domain/user/entity.go
@@ -4,16 +4,24 @@ import (
 	"github.com/google/uuid"
 )
 
+type Role string
+
+const (
+	RoleAdmin Role = "admin"
+	RoleUser  Role = "user"
+)
+
 type User struct {
 	ID           uuid.UUID `json:"id"`
 	Name         string    `json:"name"`
 	Email        string    `json:"email"`
-	PasswordHash string
+	PasswordHash string    `json:"-"`
+	Role         Role      `json:"role"`
 }
 
 type UserDTO struct {
-	ID       uuid.UUID `json:"id"`
-	Name     string    `json:"name"`
-	Email    string    `json:"email"`
-	Password string
+	ID    uuid.UUID `json:"id"`
+	Name  string    `json:"name"`
+	Email string    `json:"email"`
+	Role  Role      `json:"role"`
 }
